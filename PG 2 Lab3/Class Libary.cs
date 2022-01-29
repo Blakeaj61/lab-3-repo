62e9cc25c54547e187ec99b021af23f14376d110
Blake Johnson <BAJohnson2@student.fullsail.edu>
2022-01-29 11:32:26 -0500

    Add project files.

---
+++ PG 2 Lab3/Class Libary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+
+namespace PG_2_Lab3
+{
+    public partial class Class_Libary : Component
+    {
+        public Class_Libary()
+        {
+            InitializeComponent();
+        }
+
+        public Class_Libary(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ PG 2 Lab3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PG_2_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
